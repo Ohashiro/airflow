192d353f37d44caa563adba885f8949b90201584
emma.galliere <emma.galliere@artefact.com>
2024-12-06 12:04:08 +0100

    Add: api_timeout field and retry decorator

--- providers/src/airflow/providers/microsoft/azure/triggers/powerbi.py
+++ providers/src/airflow/providers/microsoft/azure/triggers/powerbi.py
@@ -22,7 +22,11 @@ import time
 from collections.abc import AsyncIterator
 from typing import TYPE_CHECKING
 
-from airflow.providers.microsoft.azure.hooks.powerbi import PowerBIDatasetRefreshStatus, PowerBIHook
+from airflow.providers.microsoft.azure.hooks.powerbi import (
+    PowerBIDatasetRefreshException,
+    PowerBIDatasetRefreshStatus,
+    PowerBIHook,
+)
 from airflow.triggers.base import BaseTrigger, TriggerEvent
 
 if TYPE_CHECKING:
@@ -58,6 +62,7 @@ class PowerBITrigger(BaseTrigger):
         proxies: dict | None = None,
         api_version: APIVersion | str | None = None,
         check_interval: int = 60,
+        api_timeout: float = 30,
         wait_for_termination: bool = True,
     ):
         super().__init__()
@@ -66,6 +71,7 @@ class PowerBITrigger(BaseTrigger):
         self.timeout = timeout
         self.group_id = group_id
         self.check_interval = check_interval
+        self.api_timeout = api_timeout
         self.wait_for_termination = wait_for_termination
 
     def serialize(self):
@@ -80,6 +86,7 @@ class PowerBITrigger(BaseTrigger):
                 "group_id": self.group_id,
                 "timeout": self.timeout,
                 "check_interval": self.check_interval,
+                "api_timeout": self.api_timeout,
                 "wait_for_termination": self.wait_for_termination,
             },
         )
@@ -103,6 +110,31 @@ class PowerBITrigger(BaseTrigger):
             group_id=self.group_id,
         )
 
+        def retry(api_timeout: float, time_interval: float):
+            """
+            Retry decorator for API calls based on a specified timeout.
+
+            This decorator retries the decorated function until the specified `api_timeout` is reached. If the
+            function raises a `PowerBIDatasetRefreshException`, it waits for `time_interval` seconds before retrying.
+            """
+
+            def decorator(func):
+                async def wrapped(*args, **kwargs):
+                    start_time = time.monotonic()
+                    while start_time + api_timeout > time.monotonic():
+                        try:
+                            return await func(*args, **kwargs)
+                        except PowerBIDatasetRefreshException:
+                            self.log.info("Retrying in %s seconds", time_interval)
+                            await asyncio.sleep(time_interval)
+                            pass
+                    return await func(*args, **kwargs)
+
+                return wrapped
+
+            return decorator
+
+        @retry(api_timeout=self.api_timeout, time_interval=5)
         async def fetch_refresh_status_and_error() -> tuple[str, str]:
             """Fetch the current status and error of the dataset refresh."""
             refresh_details = await self.hook.get_refresh_details_by_refresh_id(
